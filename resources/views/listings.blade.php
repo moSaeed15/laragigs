0eb03305b3da0de92edd6c8fa23d278b2fa3a9b0
Mohammed El Saeed <mohammed.elsaeed1@gmail.com>
2023-08-19 14:42:35 +0300

    added controller made tags dynamic and made card components

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,15 +1,20 @@
+@extends('layout')
+@section('content')
 
 @if(count($listings)==0)
 <p>No listings found </p>
 @endif
-
-<h1>{{$heading}}</h1>
+@include('partials._hero')
+@include('partials._search')
+<div
+class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
+>
 @foreach($listings as $listing)
-  <a href="/listing/{{$listing['id']}}"><h2>{{ $listing['title']}}</h2></a> 
-  <p>{{ $listing['description']}}</p>
+<x-listing-card :listing="$listing"/>
   @endforeach
 
-
+</div>
+@endsection
 
 
   
